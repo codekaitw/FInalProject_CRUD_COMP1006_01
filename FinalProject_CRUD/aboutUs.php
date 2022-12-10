f2070b2c4f6a815eab1d59fe0c8265f10fc580ab
codekaitw <65647662+codekaitw@users.noreply.github.com>
2022-12-10 15:08:02 -0500

    Dec 8, without reply, edit aboutUs.php

--- FinalProject_CRUD/aboutUs.php
+++ FinalProject_CRUD/aboutUs.php
@@ -5,7 +5,7 @@
 <section class="text-center aboutUs-section">
     <div class="p-3">
         <h1 class="p-1">About Us</h1>
-        <p class="lead m-auto p-1">PHP Message Board is here. We provide this website allow users to register and log in to create, update, read, delete their messages which they have.</p>
+        <p class="lead m-auto p-1">PHP Message Board is here. We provide this website allow users to register and log in to create, update, read, delete their messages which they have in their Home page.</p>
         <p class="lead p-1">
             <a href="login.php" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
